476e9cc0b5899009c3f02eaf75e2a634ef95e477
ThieenJ <thienctv03@gmail.com>
2026-08-19 07:57:45 +0700

    Update

--- src/migrations/1702000000000-drop-password-column.ts
+++ src/migrations/1702000000000-drop-password-column.ts
@@ -8,6 +8,7 @@ export class DropPasswordColumn1702000000000 implements MigrationInterface {
     await queryRunner.query('ALTER TABLE "user" DROP COLUMN IF EXISTS "password"');
   }
 
+  
   public async down(queryRunner: QueryRunner): Promise<void> {
     // Restore column if needed
     await queryRunner.query('ALTER TABLE "user" ADD COLUMN IF NOT EXISTS "password" character varying');
